2839c6693b477c8338847d6034cee62b5d42395f
Julian Villacis <julianvillacis.02@gmail.com>
2024-05-03 13:22:15 -0500

    Estructura de carpetas de arquitectura hexagonal creada.

---
+++ table_trick/src/test/java/com/riwi/table_trick/TableTrickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.table_trick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTrickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
