95227c59bef69906f3d21a46bd12ba27bfd61e40
Dayron-z <104926781+Dayron-z@users.noreply.github.com>
2024-05-19 19:19:23 -0500

    Login listo, en caso de no funcionar volver al anterior commit

--- table_trick/src/main/java/com/riwi/table_trick/api/controllers/CorsConfig.java
+++ table_trick/src/main/java/com/riwi/table_trick/api/controllers/CorsConfig.java
@@ -6,23 +6,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/sistemareservas/v1/auth/**")
+                .allowedOrigins("http://127.0.0.1:5501")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+        // Configuración CORS para otros endpoints
         registry.addMapping("/cliente/**")
                 .allowedOrigins("http://127.0.0.1:5501")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
 
         registry.addMapping("/reserva/**")
                 .allowedOrigins("http://127.0.0.1:5501")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
 
         registry.addMapping("/restaurante/**")
                 .allowedOrigins("http://127.0.0.1:5501")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
-
 }
