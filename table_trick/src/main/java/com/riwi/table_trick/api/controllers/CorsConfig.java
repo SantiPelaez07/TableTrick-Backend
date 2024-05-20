14e25f2bbad242e1151c99e4f21c13e64521c7a5
Dayron-z <104926781+Dayron-z@users.noreply.github.com>
2024-05-19 20:08:18 -0500

    Commit importante, si algo falla, retornar desde acá.
    5/19/2024 8:08pm

--- table_trick/src/main/java/com/riwi/table_trick/api/controllers/CorsConfig.java
+++ table_trick/src/main/java/com/riwi/table_trick/api/controllers/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/sistemareservas/v1/auth/**")
+        registry.addMapping("/auth/**")
                 .allowedOrigins("http://127.0.0.1:5501")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
